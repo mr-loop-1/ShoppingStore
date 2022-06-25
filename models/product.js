212ac22b6433af3facdd80ed5fd7d010e8ab4924
mr-loop-1 <loopolecreator1@gmail.com>
2022-06-25 15:33:34 +0530

    Refactored storage

--- models/product.js
+++ models/product.js
@@ -1,6 +1,18 @@
 
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
+const p = path.join(__dirname, '..', 'data', 'message.json');
+
+const readDataFromFile = (customCb) => {
+    fs.readFile(p, (err, content) => {
+        if(!err) {
+            customCb(JSON.parse(content));
+        }
+        else customCb([]);
+    }) 
+}
+ 
 module.exports = class {
 
     constructor(title) {
@@ -8,10 +20,47 @@ module.exports = class {
     }
 
     save() {
-        products.push(this);
-    }
-    static fetchAll() {
-        return products;
+        readDataFromFile(data => {
+            data.push(this);
+            fs.writeFile(p, JSON.stringify(data), err => {
+                console.log(err);
+            });
+        })
     }
 
-}
+    static fetchAll(cb) { 
+        readDataFromFile(cb);
+    }
+ 
+} 
+
+// module.exports = class {
+
+//     constructor(title) {
+//         this.title = title;
+//     }
+
+//     save() {
+        
+//         fs.readFile(p, (err, content) => {
+//             let data = [];
+//             if (!err) {
+//                 data = JSON.parse(content)
+//             }
+//             data.push(this);
+//             fs.writeFile(p, JSON.stringify(data), err => {
+//                 console.log(err);
+//             });
+//         });
+//     }
+//     static fetchAll(cb) { 
+//         fs.readFile(p, (err, content) => {
+//             if(!err) {
+//                 console.log(JSON.parse(content));
+//                 cb(JSON.parse(content));
+//             }
+//             cb([]);
+//         })
+//     }
+
+// }
