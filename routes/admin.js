b8f2b59531b33d0cf0d0fd7d61727dfbe2057ff8
mr-loop-1 <loopolecreator1@gmail.com>
2022-06-28 22:37:36 +0530

    added cart

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,18 @@
+const path = require('path');
 
 const express = require('express');
-const { getAddProduct, postAddProduct } = require('../controller/products');
+
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
